51c8048a14552d1bf3d2f1236e6581bf7df95121
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-01 14:25:37 +0530

    Update views2 in urls.py

--- WhatsappAPI/urls.py
+++ WhatsappAPI/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import bot
+from .Views2 import whatsapp_webhook, process_message
 urlpatterns = [
     path('',view=bot),
+    path('whatsapp/webhook/', whatsapp_webhook, name='whatsapp_webhook'),
+    # Add other URL patterns as needed for specific actions based on processed message
+    path('whatsapp/action/', process_message, name='process_message'),
 ]

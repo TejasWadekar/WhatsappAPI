5083a92ce580034f2f0b64a8a0f8213841049f2b
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-01 14:57:48 +0530

    Update 6 views.py

--- WhatsappAPI/views.py
+++ WhatsappAPI/views.py
@@ -13,12 +13,12 @@ def bot(request):
     message = request.POST['Body']
     sender_name = request.POST['ProfileName']
     sender_number = request.POST['From']
-    print(message, sender_name, sender_number)
+    # print(message, sender_name, sender_number)
     
-    # client.messages.create(
-    #     from_ = 'whatsapp:+14155238886',
-    #     body = f"hi {sender_name} How are you?",
-    #     to = 'whatsapp:+917058921518',
+    client.messages.create(
+        from_ = 'whatsapp:+14155238886',
+        body = f"hi {sender_name} How are you?",
+        to = 'whatsapp:+917058921518',
 
-    # )
+    )
     return HttpResponse("hello")

803a056604eee8bfd9c3e68befe7e2dd3dd15d02
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-02 14:28:24 +0530

    Update 20 views.py

--- WhatsappAPI/views.py
+++ WhatsappAPI/views.py
@@ -1,34 +1,40 @@
+import os
 from django.http import HttpResponse
-from django.shortcuts import render
-from twilio.rest import Client
 from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+# Get your Twilio account_sid and auth_token from environment variables
 account_sid = 'ACff1c255543fb69ccf7c78b54ee85d2ee'
 auth_token = '25d3bc402171e7e2840bdee6d6091237'
-client = Client(account_sid, auth_token)
 
-# Create your views here.
 @csrf_exempt
-def bot(request):
-    # print(request.POST)
-    # message = request.POST['Body']
-    # sender_name = request.POST['ProfileName']
-    # sender_number = request.POST['From']
-    # print(message, sender_name, sender_number)
+def whatsapp_reply(request):
     if request.method == 'POST':
-        if 'Body' in request.POST:
-            message = request.POST['Body']
-            # rest of your code
+        # Get the message the user sent our Twilio number
+        body = request.POST.get('Body', '')
+
+        # Start our TwiML response
+        resp = MessagingResponse()
+
+        # Determine the right reply for this message
+        if 'hello' in body.lower():
+            resp.message("Hi! How can I assist you today?")
         else:
-            print("Body not in POST data")
-    return HttpResponse("hello")
+            resp.message("Sorry, I didn't understand that. Could you please rephrase?")
 
-@csrf_exempt
-def reply(request):
-    message = client.messages.create(
-        from_ = 'whatsapp:+14155238886',
-        body = "hey there, How are you?",
-        to = 'whatsapp:+917058921518',
+        # Create a Twilio client
+        client = Client(account_sid, auth_token)
 
+        # Send a response message
+        message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body="Hey there, How are you?",
+            to='whatsapp:+917058921518'
         )
-    
-    print(message.sid)
+
+        print(message.sid)
+
+        return HttpResponse(str(resp), content_type='text/xml')
+    else:
+        return HttpResponse("Request method must be POST.")

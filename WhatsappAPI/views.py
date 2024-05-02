6f85de7bd30cd211d2f7e640038a53b1a22c2512
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-02 13:12:46 +0530

    Update 11 views.py

--- WhatsappAPI/views.py
+++ WhatsappAPI/views.py
@@ -9,23 +9,24 @@ client = Client(account_sid, auth_token)
 # Create your views here.
 @csrf_exempt
 def bot(request):
-    # print(request.POST)
-    # message = request.POST['Body']
-    # sender_name = request.POST['ProfileName']
-    sender_number = request.POST['From']
-    # print(message, sender_name, sender_number)
+    if request.method == 'POST':
+        # print(request.POST)
+        # message = request.POST['Body']
+        # sender_name = request.POST['ProfileName']
+        sender_number = request.POST['From']
+        # print(message, sender_name, sender_number)
+        
+        # client.messages.create(
+        #     from_ = 'whatsapp:+14155238886',
+        #     body = "How are you?",
+        #     to = 'whatsapp:+917058921518',
     
-    # client.messages.create(
-    #     from_ = 'whatsapp:+14155238886',
-    #     body = "How are you?",
-    #     to = 'whatsapp:+917058921518',
-
-    # )
-    message = client.messages.create(
-        from_='whatsapp:+14155238886',
-        body='hello',
-        to=sender_number,
-    )
-
-    print(message.sid)
+        # )
+        message = client.messages.create(
+            from_='whatsapp:+14155238886',
+            body='hello',
+            to=sender_number,
+        )
+    
+        print(message.sid)
     return HttpResponse("hello")

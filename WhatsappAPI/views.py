ab2db2147517d0aa72ef13a8e64e892a608d79d6
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-03 10:42:11 +0530

    error removed views.py

--- WhatsappAPI/views.py
+++ WhatsappAPI/views.py
@@ -11,6 +11,6 @@ auth_token = '25d3bc402171e7e2840bdee6d6091237'
 client = Client(account_sid, auth_token)
 
 def bot(request):
-    message = client.messages.create(body='Hello there!', from_='whatsapp:+14155238886', to='whatsapp:+917058921518')                                )
+    message = client.messages.create(body='Hello there!', from_='whatsapp:+14155238886', to='whatsapp:+917058921518')
   
     print(message.sid)

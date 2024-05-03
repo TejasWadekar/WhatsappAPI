9c61bab976bc9c9576577101454b05c4ebf4f53b
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-03 12:41:49 +0530

    removed bot url from urls.py

--- WhatsappAPI/urls.py
+++ WhatsappAPI/urls.py
@@ -5,7 +5,6 @@ from .views3 import whatsapp_webhook
 
 
 urlpatterns = [
-    path('',view=bot),
     path('whatsapp/webhook/', whatsapp_webhook, name='whatsapp_webhook'),
     # Add other URL patterns as needed for specific actions based on processed message
     path('whatsapp/action/', process_message, name='process_message'),

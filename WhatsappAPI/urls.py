2c9ad0de5e6bce2aa96c864dca50b981bfffc3f8
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-03 11:02:08 +0530

    removed reply from urls.py

--- WhatsappAPI/urls.py
+++ WhatsappAPI/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('whatsapp/webhook/', whatsapp_webhook, name='whatsapp_webhook'),
     # Add other URL patterns as needed for specific actions based on processed message
     path('whatsapp/action/', process_message, name='process_message'),
-    path('reply/', reply, name='reply'),
+
 
     path('webhook',view = whatsapp_webhook, name='whatsapp_webhook'),
 ]

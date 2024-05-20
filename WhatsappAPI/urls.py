3688d782618ef3108cb43593488d6515fb378b39
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-20 13:27:14 +0530

    Update urls.py 12

--- WhatsappAPI/urls.py
+++ WhatsappAPI/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import bot
-from .view11M import whatsapp_webhook, send_first_message
+from .views12 import whatsapp_webhook, send_first_message
 
 urlpatterns = [
     path('webhook', whatsapp_webhook, name='whatsapp_webhook'),

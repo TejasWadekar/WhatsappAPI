18a30b75c43c36c2b762a495cf1e0244378a5776
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-20 13:18:52 +0530

    Update urls.py

--- WhatsappAPI/urls.py
+++ WhatsappAPI/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from .views import bot
-from .Views2 import whatsapp_webhook, process_message
+
 from .view11M import whatsapp_webhook, send_first_message
 
 
 urlpatterns = [
-    path('whatsapp/webhook/', whatsapp_webhook, name='whatsapp_webhook'),
-    # Add other URL patterns as needed for specific actions based on processed message
-    path('whatsapp/action/', process_message, name='process_message'),
-
 
     path('webhook',view = whatsapp_webhook, name='whatsapp_webhook'),
     path('api/send_first_message/', view = send_first_message, name = "sfm"),

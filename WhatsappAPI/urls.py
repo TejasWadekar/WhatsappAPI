22f7da5479d161b5222285c0b90d7b9181c0b9c7
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-05-20 13:26:04 +0530

    Update urls.py new

--- WhatsappAPI/urls.py
+++ WhatsappAPI/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from .views import bot
-
 from .view11M import whatsapp_webhook, send_first_message
 
-
 urlpatterns = [
-
-    path('webhook',view = whatsapp_webhook, name='whatsapp_webhook'),
-    path('api/send_first_message/', view = send_first_message, name = "sfm"),
+    path('webhook', whatsapp_webhook, name='whatsapp_webhook'),
+    path('api/send_first_message/', send_first_message, name="sfm"),
 ]
